4d04fe8507d759dfd059feec156e165106088bda
irimon <ilanrimon@yahoo.com>
2014-10-18 07:35:38 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TalkWithGrandma::Application.config.secret_key_base = '13bd99309e0bee37f6083aa35f7b1d5dafe8e2645348d0487feaf39d2727d5376488d05d975bab171e25fa7bd8c6e0e74342501ba072b125e81e1d6ad70ed7d4'
